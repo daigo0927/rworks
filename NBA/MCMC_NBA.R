ba03474144f51d9fc8e220875fd2e42b0199f527
Daigo HIROOKA <tappy.hello-planet@ezweb.ne.jp>
2016-11-14 00:26:40 +0900

    add MCMC code

--- NBA/MCMC_NBA.R
+++ NBA/MCMC_NBA.R
@@ -1,3 +1,4 @@
+set.seed(7)
 # run MCMC for NBA data Analysis
 # estimate parameter of some score
 
@@ -90,40 +91,191 @@ part_time <- apply(
 part_time <- t(part_time)
 rownames(part_time) <- team_member[[t_idx]]
 
+
+# confirm distribution shape ---------------------------------
+plot(x <- seq(0, 20, 0.01)
+     , y <- dgamma(x, shape=10 , scale=0.3)
+     # , y <- dnorm(x, mean=3, sd=1)
+     , xlim=c(min(x),max(x))
+     , type='l'
+)
+
+
 # code for MCMC -------------------------------------------------------
 t_idx <- 6
+# score list focused to t_idx team
 score_list <- lapply(htoh_i_j[[t_idx]], trans_time)
 
-# sampling process list
+
+# preprocessing --------------------------------------------------
+# sampling result list-------
 NBA_MCMC_res <- list()
-# parameter to be estimated
+# define parameters to be estimated
 # mu : general performance of the player
 # lambda : precision for opponent team
 # alpha_i : plus component for i-th opponent
 parameter_name <- c('mu', 'lambda', paste0('alpha_',as.character(seq(1,30))))
-sample_num <- 10000
+
+# sampling length ---------------
+sample_num <- 5000
+
+# initialize parameters to be estimated ---------------------
+parameter_init <- c(10, 1, runif(30, -1, 1))
+names(parameter_init) <- parameter_name
+
+# initialize result list
 for(i in 1:length(parameter_name)){
-  NBA_MCMC_res[[i]] <- matrix(0, nrow=sample_num, ncol=length(team_member[[t_idx]]))
+  NBA_MCMC_res[[i]] <- matrix(0, nrow=sample_num+1, ncol=length(team_member[[t_idx]]))
   colnames(NBA_MCMC_res[[i]]) <- team_member[[t_idx]]
+  NBA_MCMC_res[[i]][1,] <- parameter_init[i]
 }
 names(NBA_MCMC_res) <- parameter_name
 
-# gibbs sampling 10000 iteration
+# prior distribution for mu : general performance 
+# and prior ditribution for lambda : precision for opponent team
+# informationless gamma distribution 
+k_hyper <- 1
+theta_hyper <- 1000
+
+# prior distribution for alpha_i : plus component for i-th opponent
+lambda_i_hyper <- 0.001
+sigma_set <- 1 # given variance for likelihood
+
+
+# gibbs sampling 5000 iteration-----------------------------------
 # for each member
-apply(
-  as.matrix(team_member[[t_idx]])
-  , 1
-  , function(x){
-    for(i in 1:sample_num){
-      
+for(x in team_member[[t_idx]]){
+  
+  # x : member name
+  for(i in 1:sample_num){
+    x <- as.character(x)
+    
+    # get recent sample
+    recent_parameter <- sapply(NBA_MCMC_res, function(y){
+      return(y[i,x])
+    })
+    mu_i <- recent_parameter[1]
+    lambda_i <- recent_parameter[2]
+    alpha_ij <- recent_parameter[-c(1,2)]
+    
+    
+    # score_list focused to x member
+    s_list_p <- lapply(score_list, function(y){
+      return(y[which(y$Player==x),])
+    })
+    
+    # PTS obtained from j-th team
+    y_ij <- sapply(s_list_p, function(y){
+      if(is.null(y)==T) return(0)
+      else return(sum(na.omit(y$PTS)))
+    })
+    
+    # quarters performed between j-th team
+    n_q_ij <- sapply(s_list_p, function(y){
+      if(is.null(y)==T) return(0)
+      else return(sum(na.omit(y$MIN))/12)
+    })
+    
+    # learning general performance -------------------------
+    # update mu distribution
+    k1_new <- k_hyper + sum(y_ij - n_q_ij*alpha_ij)
+    theta1_new <- (1/theta_hyper + sum(n_q_ij))^(-1)
+    # general performance mu sampling
+    mu_sample <- rgamma(1, shape=k1_new, scale=theta1_new)
+    
+    # learning precision for opponent team --------------------
+    # 29 : team number except for oneself
+    k2_new <- k_hyper + 29/2
+    theta2_new <- (1/theta_hyper + 29/2*sd(alpha_ij[-t_idx])^2)^(-1)
+    # precision lambda sampling
+    lambda_sample <- rgamma(1, shape=k2_new, scale=theta2_new)
+    
+    # learning plus component for j-th opponent -----------------
+    alpha_sample <- rep(0, 30)
+    lam_set <- 1/(sigma_set^2)
+    mu_ML <- (y_ij - n_q_ij*mu_i)/n_q_ij
+    
+    lambda_new <- n_q_ij*lam_set+lambda_i
+    mu_new <- n_q_ij*lam_set*mu_ML/lambda_new
+    mu_new[which(is.nan(mu_new)==T)] <- 0
+    
+    # plus component alpha sampling
+    for(j in 1:30){
+      if(j==t_idx){next}
+      alpha_sample[j] <- rnorm(1, mean=mu_new[j], sd=1/sqrt(lambda_new[j]))
     }
+    
+    
+    # record sampling result
+    NBA_MCMC_res$mu[i+1,x] <- mu_sample
+    NBA_MCMC_res$lambda[i+1, x] <- lambda_sample
+    for (j in 1:30){
+      NBA_MCMC_res[[j+2]][i+1, x] <- alpha_sample[j]
+    }
+    
   }
+  
+  print(paste0(x, ' sampling completed'))
+}
+
+
+# visualize MCMC result ----------------------------------------
+burn_in <- 1:1001
+
+# MCMC sampling process
+dev.off()
+p_name <- team_member[[t_idx]][7]
+plot(
+  x <- 1:sample_num+1
+  , y <- NBA_MCMC_res[['mu']][x, p_name]
+  , type='l'
+  , xlab='sampling iteration'
+  , ylab=paste('PTS of', p_name)
+  , main='MCMC sampling process'
 )
 
+# pararell plot of mu : general performance
+par(mfrow=c(4,1))
+for(i in c(3,7,13,18)){
+  p_name <- team_member[[t_idx]][i]
+  plot(
+    density(NBA_MCMC_res[['mu']][-burn_in, p_name])
+    , xlab='mu'
+    , main=paste('general performance of', p_name)
+    , xlim=c(0,10)
+  )
+  rug(NBA_MCMC_res[['mu']][-burn_in, p_name])
+}
 
+# pararell plot of lambda : precision for opponent team
+par(mfrow=c(4,1))
+for(i in c(1,7,13,18)){
+  p_name <- team_member[[t_idx]][i]
+  plot(
+    density(1/NBA_MCMC_res[['lambda']][-burn_in,p_name])
+    , xlab='variance'
+    , main = paste('variance for opponents of', p_name)
+    , xlim=c(0,10)
+    )
+  rug(1/NBA_MCMC_res[['lambda']][-burn_in, p_name])
+}
 
-
-
+# pararell plot of alpha : compatibility for each team
+par(mfrow=c(4,1))
+# p_name <- 'LeBron James'
+# p_name <- team_member[[t_idx]][18]
+p_name <- 'Cleveland Cavaliers'
+for(i in c(1, 10, 20, 30)){
+  param <- paste0('alpha_' ,as.character(i))
+  plot(
+    density(NBA_MCMC_res[[param]][-burn_in, p_name])
+    , xlab = 'compatibility'
+    , main = paste0(p_name, ' compatibility for ',as.character(team_name[i]))
+    , xlim = c(-2,2)
+    # , xlim = c(-3,3)
+  )
+  rug(NBA_MCMC_res[[param]][-burn_in, p_name])
+}
 
 
 
--- NBA/import_NBAmember.R
+++ NBA/import_NBAmember.R
@@ -123,166 +123,166 @@ names(htoh_i) <- as.character(team_name)
 
 
 
-# read 2016 regular season box score *********************************************
-regular_score <- read.csv("../Daigo/Desktop/NBA/2016/score_r.csv", header=T)
-playoffs_score <- read.csv('../Daigo/Desktop/NBA/2016/score_p.csv', header=T)
-
-# use FGM, 3PM, FTM data
-point_score_name <- c('Player1', 'FGM1', 'FGA1', 'FG.1', 'X3PM1', 'X3PA1', 'X3P.1', 'FTM1', 'FTA1', 'FT.1', 'Player2', 'FGM2', 'FGA2', 'FG.2', 'X3PM2', 'X3PA2', 'X3P.2', 'FTM2', 'FTA2', 'FT.2')
-point_score_col <- match(point_score_name, colnames(regular_score))
-
-point_score_r <- regular_score[,point_score_col]
-colnames(point_score_r) <- c('Player1', 'FGM1', 'FGA1', 'FGper1', 'TPM1', 'TPA1', 'TPper1', 'FTM1', 'FTA1', 'FTper1', 'Player2', 'FGM2', 'FGA2', 'FGper2', 'TPM2', 'TPA2', 'TPper2', 'FTM2' ,'FTA2', 'FTper2')
-point_score_r$state <- as.numeric(point_score_r$Player2)
-
-point_score_p <- playoffs_score[,point_score_col]
-colnames(point_score_p) <- c('Player1', 'FGM1', 'FGA1', 'FGper1', 'TPM1', 'TPA1', 'TPper1', 'FTM1', 'FTA1', 'FTper1', 'Player2', 'FGM2', 'FGA2', 'FGper2', 'TPM2', 'TPA2', 'TPper2', 'FTM2', 'FTA2', 'FTper2')
-for(i in 1:nrow(point_score_p)){
-  point_score_p$state[i] <- which(as.character(point_score_p$Player2[i])==team_name)
-}
-
-
-# change FGM to 2 point score
-point_score_r[,'FGM1'] <- point_score_r[,'FGM1'] - point_score_r[,'TPM1']
-point_score_r[,'FGA1'] <- point_score_r[,'FGA1'] - point_score_r[,'TPA1']
-point_score_r[,'FGM2'] <- point_score_r[,'FGM2'] - point_score_r[,'TPM2']
-point_score_r[,'FGA2'] <- point_score_r[,'FGA2'] - point_score_r[,'TPA2']
-point_score_r[,'FGper1'] <- point_score_r[,'FGM1'] / point_score_r[,'FGA1']*100
-point_score_r[,'FGper2'] <- point_score_r[,'FGM2'] / point_score_r[,'FGA2']*100
-
-
-point_score_p[,'FGM1'] <- point_score_p[,'FGM1'] - point_score_p[,'TPM1']
-point_score_p[,'FGA1'] <- point_score_p[,'FGA1'] - point_score_p[,'TPA1']
-point_score_p[,'FGM2'] <- point_score_p[,'FGM2'] - point_score_p[,'TPM2']
-point_score_p[,'FGA2'] <- point_score_p[,'FGA2'] - point_score_p[,'TPA2']
-point_score_p[,'FGper1'] <- point_score_p[,'FGM1'] / point_score_p[,'FGA1']*100
-point_score_p[,'FGper2'] <- point_score_p[,'FGM2'] / point_score_p[,'FGA2']*100
-
-
-
-
-team_name <- sort(unique(point_score_r$Player1))
-
-
-# make point score list by each team ---------------------------------------------------------
-point_by_team <- list()
-for(i in 1:length(team_name)){
-  
-  # matches the i-th team joined
-  game_num <- sort(append(grep(team_name[i], point_score_r[,'Player1']), grep(team_name[i], point_score_r[,'Player2'])))
-  
-  point_by_team[[i]] <- matrix(0, nrow=length(game_num), ncol=12)
-  colnames(point_by_team[[i]]) <- c('Player', 'FGM', 'FGA', 'FGper', 'TPM', 'TPA', 'TPper', 'FTM', 'FTA', 'FTper', 'Opponent', 'state')
-  
-  for(j in 1:length(game_num)){
-    # Player
-    point_by_team[[i]][j,1] <- team_name[i]
-    
-    if(point_score_r$Player1[game_num[j]] == team_name[i]){
-      # FGM
-      point_by_team[[i]][j,'FGM'] <- point_score_r[game_num[j], 'FGM1']
-      # FGA
-      point_by_team[[i]][j,'FGA'] <- point_score_r[game_num[j], 'FGA1']
-      # FGper
-      point_by_team[[i]][j,'FGper'] <- point_score_r[game_num[j], 'FGper1']
-      # 3PM
-      point_by_team[[i]][j,'TPM'] <- point_score_r[game_num[j], 'TPM1']
-      # 3PA
-      point_by_team[[i]][j,'TPA'] <- point_score_r[game_num[j], 'TPA1']
-      # 3Pper
-      point_by_team[[i]][j,'TPper'] <- point_score_r[game_num[j], 'TPper1']
-      # FTM
-      point_by_team[[i]][j,'FTM'] <- point_score_r[game_num[j], 'FTM1']
-      # FTA
-      point_by_team[[i]][j,'FTA'] <- point_score_r[game_num[j], 'FTA1']
-      # FTper
-      point_by_team[[i]][j,'FTper'] <- point_score_r[game_num[j], 'FTper1']
-      # Opponent
-      point_by_team[[i]][j,'Opponent'] <- point_score_r[game_num[j], 'Player2']
-      
-    }else if(point_score_r$Player2[game_num[j]] == team_name[i]){
-      # FGM
-      point_by_team[[i]][j,'FGM'] <- point_score_r[game_num[j], 'FGM2']
-      # FGA
-      point_by_team[[i]][j,'FGA'] <- point_score_r[game_num[j], 'FGA2']
-      # FGper
-      point_by_team[[i]][j,'FGper'] <- point_score_r[game_num[j], 'FGper2']
-      # TPM
-      point_by_team[[i]][j,'TPM'] <- point_score_r[game_num[j], 'TPM2']
-      # TPM
-      point_by_team[[i]][j,'TPA'] <- point_score_r[game_num[j], 'TPA2']
-      # TPper
-      point_by_team[[i]][j,'TPper'] <- point_score_r[game_num[j], 'TPper2']
-      # TPM
-      point_by_team[[i]][j,'FTM'] <- point_score_r[game_num[j], 'FTM2']
-      # TPM
-      point_by_team[[i]][j,'FTA'] <- point_score_r[game_num[j], 'FTA2']
-      # TPper
-      point_by_team[[i]][j,'FTper'] <- point_score_r[game_num[j], 'FTper2']
-      # Opponent
-      point_by_team[[i]][j,'Opponent'] <- point_score_r[game_num[j], 'Player1']
-    }
-    # state
-    point_by_team[[i]][j,'state'] <- point_score_r[game_num[j], 'state']
-  }
-}
-names(point_by_team) <- team_name
-
-
-# make point list, head to head ------------------------------------------
-# head_to_head[[one]][[opponent]]
-head_to_head <- list()
-for(i in 1:30){
-  head_to_head[[i]] <- list()
-  for(j in 1:30){
-    head_to_head[[i]][[j]] <- point_by_team[[i]][which(point_by_team[[i]][,'Opponent']==j), ] 
-  }
-}
-names(head_to_head) <- team_name
-
-
-# make point score matrix team by team for each score( FGM, 3PM, FTM) -----------------------------------
-# matching number of times matrix
-# because of after division, diagonal elements are set 1
-match_num <- diag(1, nrow=30, ncol=30)
-# score matrix list, composed by each point score(FGM, 3PM, FTM) matrix
-score_sum_matrix <- list(matrix(0,30,30), matrix(0,30,30), matrix(0,30,30), matrix(0,30,30), matrix(0,30,30), matrix(0,30,30), matrix(0,30,30), matrix(0,30,30), matrix(0,30,30))
-names(score_sum_matrix) <- c('FGM', 'FGA', 'FGper', 'TPM', 'TPA', 'TPper', 'FTM', 'FTA', 'FTper')
-
-# input component of score_sum_matrix
-for(i in 1:30){
-  for(j in 1:30){
-    table <- head_to_head[[i]][[j]]
-    score_i_j <- apply(table[,-which(colnames(table)==c('Player', 'Opponent', 'state'))], 2, sum)
-    for(k in 1:length(score_name)){
-      score_sum_matrix[[k]][j,i] <- score_i_j[score_name[k]]
-    }
-    
-    # input i vs j match_num
-    match_num[j,i] <- match_num[j,i]+nrow(head_to_head[[i]][[j]])
-  }
-}
-
-# sort playoffs result -------------------------------------------
-cname1 <- colnames(head_to_head[[1]][[1]])
-cname1[-which(cname1=='state')] <- paste0(cname1[-which(cname1=='state')],'1')
-cname1[which(cname1=='Opponent1')] <- 'Player2'
-cname2 <- colnames(head_to_head[[1]][[1]])
-cname2[-which(cname2=='state')] <- paste0(cname2[-which(cname2=='state')],'2')
-cname2[which(cname2=='Opponent2')] <- 'Player1'
-score1 <- point_score_p[,cname1]
-score2 <- point_score_p[,cname2]
-
-sort_score_p <- matrix(0, nrow=2*nrow(point_score_p), ncol=ncol(head_to_head[[1]][[1]]) )
-cname <- colnames(head_to_head[[1]][[1]])
-colnames(score1) <- cname
-colnames(score2) <- cname
-for(i in 1:nrow(score1)){
-  sort_score_p[2*i-1,] <- t(score1[i,])
-  sort_score_p[2*i,] <- t(score2[i,])
-}
-colnames(sort_score_p) <- cname
-
+# # read 2016 regular season box score *********************************************
+# regular_score <- read.csv("../Daigo/Desktop/NBA/2016/score_r.csv", header=T)
+# playoffs_score <- read.csv('../Daigo/Desktop/NBA/2016/score_p.csv', header=T)
+# 
+# # use FGM, 3PM, FTM data
+# point_score_name <- c('Player1', 'FGM1', 'FGA1', 'FG.1', 'X3PM1', 'X3PA1', 'X3P.1', 'FTM1', 'FTA1', 'FT.1', 'Player2', 'FGM2', 'FGA2', 'FG.2', 'X3PM2', 'X3PA2', 'X3P.2', 'FTM2', 'FTA2', 'FT.2')
+# point_score_col <- match(point_score_name, colnames(regular_score))
+# 
+# point_score_r <- regular_score[,point_score_col]
+# colnames(point_score_r) <- c('Player1', 'FGM1', 'FGA1', 'FGper1', 'TPM1', 'TPA1', 'TPper1', 'FTM1', 'FTA1', 'FTper1', 'Player2', 'FGM2', 'FGA2', 'FGper2', 'TPM2', 'TPA2', 'TPper2', 'FTM2' ,'FTA2', 'FTper2')
+# point_score_r$state <- as.numeric(point_score_r$Player2)
+# 
+# point_score_p <- playoffs_score[,point_score_col]
+# colnames(point_score_p) <- c('Player1', 'FGM1', 'FGA1', 'FGper1', 'TPM1', 'TPA1', 'TPper1', 'FTM1', 'FTA1', 'FTper1', 'Player2', 'FGM2', 'FGA2', 'FGper2', 'TPM2', 'TPA2', 'TPper2', 'FTM2', 'FTA2', 'FTper2')
+# for(i in 1:nrow(point_score_p)){
+#   point_score_p$state[i] <- which(as.character(point_score_p$Player2[i])==team_name)
+# }
+# 
+# 
+# # change FGM to 2 point score
+# point_score_r[,'FGM1'] <- point_score_r[,'FGM1'] - point_score_r[,'TPM1']
+# point_score_r[,'FGA1'] <- point_score_r[,'FGA1'] - point_score_r[,'TPA1']
+# point_score_r[,'FGM2'] <- point_score_r[,'FGM2'] - point_score_r[,'TPM2']
+# point_score_r[,'FGA2'] <- point_score_r[,'FGA2'] - point_score_r[,'TPA2']
+# point_score_r[,'FGper1'] <- point_score_r[,'FGM1'] / point_score_r[,'FGA1']*100
+# point_score_r[,'FGper2'] <- point_score_r[,'FGM2'] / point_score_r[,'FGA2']*100
+# 
+# 
+# point_score_p[,'FGM1'] <- point_score_p[,'FGM1'] - point_score_p[,'TPM1']
+# point_score_p[,'FGA1'] <- point_score_p[,'FGA1'] - point_score_p[,'TPA1']
+# point_score_p[,'FGM2'] <- point_score_p[,'FGM2'] - point_score_p[,'TPM2']
+# point_score_p[,'FGA2'] <- point_score_p[,'FGA2'] - point_score_p[,'TPA2']
+# point_score_p[,'FGper1'] <- point_score_p[,'FGM1'] / point_score_p[,'FGA1']*100
+# point_score_p[,'FGper2'] <- point_score_p[,'FGM2'] / point_score_p[,'FGA2']*100
+# 
+# 
+# 
+# 
+# team_name <- sort(unique(point_score_r$Player1))
+# 
+# 
+# # make point score list by each team ---------------------------------------------------------
+# point_by_team <- list()
+# for(i in 1:length(team_name)){
+#   
+#   # matches the i-th team joined
+#   game_num <- sort(append(grep(team_name[i], point_score_r[,'Player1']), grep(team_name[i], point_score_r[,'Player2'])))
+#   
+#   point_by_team[[i]] <- matrix(0, nrow=length(game_num), ncol=12)
+#   colnames(point_by_team[[i]]) <- c('Player', 'FGM', 'FGA', 'FGper', 'TPM', 'TPA', 'TPper', 'FTM', 'FTA', 'FTper', 'Opponent', 'state')
+#   
+#   for(j in 1:length(game_num)){
+#     # Player
+#     point_by_team[[i]][j,1] <- team_name[i]
+#     
+#     if(point_score_r$Player1[game_num[j]] == team_name[i]){
+#       # FGM
+#       point_by_team[[i]][j,'FGM'] <- point_score_r[game_num[j], 'FGM1']
+#       # FGA
+#       point_by_team[[i]][j,'FGA'] <- point_score_r[game_num[j], 'FGA1']
+#       # FGper
+#       point_by_team[[i]][j,'FGper'] <- point_score_r[game_num[j], 'FGper1']
+#       # 3PM
+#       point_by_team[[i]][j,'TPM'] <- point_score_r[game_num[j], 'TPM1']
+#       # 3PA
+#       point_by_team[[i]][j,'TPA'] <- point_score_r[game_num[j], 'TPA1']
+#       # 3Pper
+#       point_by_team[[i]][j,'TPper'] <- point_score_r[game_num[j], 'TPper1']
+#       # FTM
+#       point_by_team[[i]][j,'FTM'] <- point_score_r[game_num[j], 'FTM1']
+#       # FTA
+#       point_by_team[[i]][j,'FTA'] <- point_score_r[game_num[j], 'FTA1']
+#       # FTper
+#       point_by_team[[i]][j,'FTper'] <- point_score_r[game_num[j], 'FTper1']
+#       # Opponent
+#       point_by_team[[i]][j,'Opponent'] <- point_score_r[game_num[j], 'Player2']
+#       
+#     }else if(point_score_r$Player2[game_num[j]] == team_name[i]){
+#       # FGM
+#       point_by_team[[i]][j,'FGM'] <- point_score_r[game_num[j], 'FGM2']
+#       # FGA
+#       point_by_team[[i]][j,'FGA'] <- point_score_r[game_num[j], 'FGA2']
+#       # FGper
+#       point_by_team[[i]][j,'FGper'] <- point_score_r[game_num[j], 'FGper2']
+#       # TPM
+#       point_by_team[[i]][j,'TPM'] <- point_score_r[game_num[j], 'TPM2']
+#       # TPM
+#       point_by_team[[i]][j,'TPA'] <- point_score_r[game_num[j], 'TPA2']
+#       # TPper
+#       point_by_team[[i]][j,'TPper'] <- point_score_r[game_num[j], 'TPper2']
+#       # TPM
+#       point_by_team[[i]][j,'FTM'] <- point_score_r[game_num[j], 'FTM2']
+#       # TPM
+#       point_by_team[[i]][j,'FTA'] <- point_score_r[game_num[j], 'FTA2']
+#       # TPper
+#       point_by_team[[i]][j,'FTper'] <- point_score_r[game_num[j], 'FTper2']
+#       # Opponent
+#       point_by_team[[i]][j,'Opponent'] <- point_score_r[game_num[j], 'Player1']
+#     }
+#     # state
+#     point_by_team[[i]][j,'state'] <- point_score_r[game_num[j], 'state']
+#   }
+# }
+# names(point_by_team) <- team_name
+# 
+# 
+# # make point list, head to head ------------------------------------------
+# # head_to_head[[one]][[opponent]]
+# head_to_head <- list()
+# for(i in 1:30){
+#   head_to_head[[i]] <- list()
+#   for(j in 1:30){
+#     head_to_head[[i]][[j]] <- point_by_team[[i]][which(point_by_team[[i]][,'Opponent']==j), ] 
+#   }
+# }
+# names(head_to_head) <- team_name
+# 
+# 
+# # make point score matrix team by team for each score( FGM, 3PM, FTM) -----------------------------------
+# # matching number of times matrix
+# # because of after division, diagonal elements are set 1
+# match_num <- diag(1, nrow=30, ncol=30)
+# # score matrix list, composed by each point score(FGM, 3PM, FTM) matrix
+# score_sum_matrix <- list(matrix(0,30,30), matrix(0,30,30), matrix(0,30,30), matrix(0,30,30), matrix(0,30,30), matrix(0,30,30), matrix(0,30,30), matrix(0,30,30), matrix(0,30,30))
+# names(score_sum_matrix) <- c('FGM', 'FGA', 'FGper', 'TPM', 'TPA', 'TPper', 'FTM', 'FTA', 'FTper')
+# 
+# # input component of score_sum_matrix
+# for(i in 1:30){
+#   for(j in 1:30){
+#     table <- head_to_head[[i]][[j]]
+#     score_i_j <- apply(table[,-which(colnames(table)==c('Player', 'Opponent', 'state'))], 2, sum)
+#     for(k in 1:length(score_name)){
+#       score_sum_matrix[[k]][j,i] <- score_i_j[score_name[k]]
+#     }
+#     
+#     # input i vs j match_num
+#     match_num[j,i] <- match_num[j,i]+nrow(head_to_head[[i]][[j]])
+#   }
+# }
+# 
+# # sort playoffs result -------------------------------------------
+# cname1 <- colnames(head_to_head[[1]][[1]])
+# cname1[-which(cname1=='state')] <- paste0(cname1[-which(cname1=='state')],'1')
+# cname1[which(cname1=='Opponent1')] <- 'Player2'
+# cname2 <- colnames(head_to_head[[1]][[1]])
+# cname2[-which(cname2=='state')] <- paste0(cname2[-which(cname2=='state')],'2')
+# cname2[which(cname2=='Opponent2')] <- 'Player1'
+# score1 <- point_score_p[,cname1]
+# score2 <- point_score_p[,cname2]
+# 
+# sort_score_p <- matrix(0, nrow=2*nrow(point_score_p), ncol=ncol(head_to_head[[1]][[1]]) )
+# cname <- colnames(head_to_head[[1]][[1]])
+# colnames(score1) <- cname
+# colnames(score2) <- cname
+# for(i in 1:nrow(score1)){
+#   sort_score_p[2*i-1,] <- t(score1[i,])
+#   sort_score_p[2*i,] <- t(score2[i,])
+# }
+# colnames(sort_score_p) <- cname
+# 
 
 
 
